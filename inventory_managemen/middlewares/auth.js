ad490694923bb36826b1a6fb90702ce559efd532
ahmed ehab <ahmedehab4778@gmail.com>
2026-02-13 20:24:20 +0200

    create user api add auth

--- inventory_managemen/middlewares/auth.js
+++ inventory_managemen/middlewares/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const catchError = require('../helpers/catchError');
 const User = require('../models/user');
 const AppError = require('../models/user');
@@ -6,6 +7,10 @@ const auth = catchError(async (req, res, next) => {
   const {userName, password} = req.body;
   const user = await User.findOne({userName, password});
   if (!user) {
+    throw new AppError('userName or password is wrong ');
+  }
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
     throw new AppError('userName or password is wrong ');
   }
   req.logginUser = user;
